12e979f84ab491b0dc92aed8fd9372fc6ad8ccf8
jiang <emailtojiang@gmail.com>
2024-06-30 05:38:13 +0800

    时隔多日，发现又学了很多新东西，直接替换老技术了

--- src/components/public/ImageSliderComponent.vue
+++ src/components/public/ImageSliderComponent.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="image-slider">
-    <div class="image-container" :style="containerStyle">
-      <div v-for="(image, index) in images" :key="index"
-           class="image" :style="{ 'background-image': 'url(' + image + ')' }"/>
-    </div>
-    <div class="dots" v-show="images.length > 1">
-      <span v-for="(image, index) in images" :key="index"
-            :class="{ active: currentIndex === index }"
-            @click="switchImage(index)"/>
-    </div>
-  </div>
+  <n-carousel show-arrow autoplay draggable>
+    <n-image class="image-container" lazy
+             v-for="(image, index) in images" :key="index" :src="image"/>
+  </n-carousel>
 </template>
 
 <script>
+import {NCarousel, NImage, NImageGroup} from "naive-ui";
+
 export default {
+  components: {
+    NCarousel,
+    NImage,
+    NImageGroup
+  },
   data() {
     return {
       currentIndex: 0,
@@ -25,63 +25,13 @@ export default {
       required: true,
     },
   },
-  computed: {
-    containerStyle() {
-      return {
-        transform: `translateX(-${this.currentIndex * 100}%)`,
-        transition: 'transform 0.5s',
-      };
-    },
-  },
-  methods: {
-    switchImage(index) {
-      this.currentIndex = index;
-    },
-  },
 };
 </script>
 
 <style scoped>
-.image-slider {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-}
-
 .image-container {
   display: flex;
   width: 100%;
   height: 100%;
-}
-
-.image {
-  flex-shrink: 0;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  background-position: center;
-}
-
-.dots {
-  position: absolute;
-  bottom: 10px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.dots span {
-  width: 10px;
-  height: 10px;
-  margin: 0 5px;
-  background: #ccc;
-  border-radius: 50%;
-  cursor: pointer;
-}
-
-.dots .active {
-  background: #333;
 }
 </style>
